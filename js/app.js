8b6d7ebe2f6a4c202caa03e105742cfe3695f6b0
ebnanzhran <bnanzhran515@gmail.com>
2022-04-24 23:43:39 +0300

    edited

--- js/app.js
+++ js/app.js
@@ -1,258 +1,147 @@
-let sectionEl1=document.getElementById("form");
-let main=document.getElementById("main");
-let heading=document.createElement("h1")
-heading.textContent="Welcome to System "
-heading.style.textAlign="Center"
-sectionEl1.appendChild(heading)
-sectionEl1.setAttribute("backgroundColor","black")
-sectionEl1.style.textAlign="center"
+'use strict'
+let allEmp=[]
+let sectionEl=document.getElementById("showCard")
+let fullName=document.getElementById("fullname")
+let imgph=document.getElementById("imgph")
 
-//  main.style.textAlign="center"
-let newbar=document.createElement("br")
-main.append(newbar)
-
-let sectionEl2=document.createElement("section")
-sectionEl2.setAttribute("id","form2")
-main.appendChild(sectionEl2)
-sectionEl2.style.textAlign="center"
-let newbar2=document.createElement("br")
-main.append(newbar2)
-
-let sectionEl3=document.createElement("section")
-sectionEl3.setAttribute("id","form3")
-main.appendChild(sectionEl3)
-sectionEl3.style.textAlign="center"
-
-let newbar3=document.createElement("br")
-main.append(newbar3)
-
-let nav1=document.createElement("nav")
-nav1.setAttribute("id","input1")
-main.appendChild(nav1)
-nav1.style.textAlign="center"
-
-
-let newbar4=document.createElement("br")
-main.append(newbar4)
-
-
-
-let nav2=document.createElement("nav")
-nav2.setAttribute("id","input1")
-main.appendChild(nav2)
-nav2.style.textAlign="center"
-
-let inputName;
-  inputName =document.createElement("INPUT");
-  inputName.setAttribute("type","text");
-  inputName.setAttribute("value","Enter your name");
-sectionEl1.appendChild(inputName)
-
-let inputText
-function creatInputText(){
-inputText=document.createElement("INPUT")
-inputText.setAttribute("type","text");
-inputText.setAttribute("value","Input Image URL")
-nav1.appendChild(inputText)
+function Employee(id, name, department, level, image, salary) {
+  this.employeeId = id;
+  this.fullName = name;
+  this.department = department;
+  this.level = level;
+  this.image = image;
+  this.salary = salary;
+  allEmp.push(this);
 
 }
 
-let btn;
-let noteBtn
-var x
-var x1
-var dept;
-var level;
-var img;
-var nameCard;
-var arr=[];
-var salary;
-var i;
-var id=1000;
-function createButton(){
- btn=document.createElement("BUTTON")
- noteBtn=document.createTextNode("Submit")
- btn.appendChild(noteBtn)
- btn.addEventListener("click",alt)
- nav2.appendChild(btn)
+function getRndInteger(min,max){
+  return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+Employee.prototype.NewSalary11=function () {
+  if(this.level=="Senior"){
+    this.salary=getRndInteger(1500,2000)*(1-7.5)*(-1);
+    
+  }
+  if(this.level=="Mid-Senior"){
+    this.salary=getRndInteger(1000,1500)*(1-7.5)*(-1);
+    
+  }
+  if(this.level=="Jonir"){
+    this.salary=getRndInteger(500,1000)*(1-7.5)*(-1);
+    
+  }
+}
+
+
+Employee.prototype.render= function(){
+  let card = document.createElement('div')
+  card.style.textAlign="center"
+  card.style.background="gray"
+  card.style.width="150px"
+  card.style.marginLeft="45%"
+  // card.classList = ('cardDiv')
+  sectionEl.appendChild(card)
+  // create image for employee
+  let imageEl = document.createElement('img');
+  // imageEl.classList = ('card-img-top');
+  imageEl.style.width="50px"
+  imageEl.src = this.image;
+  imageEl.style.padding="10px"
+  imageEl.alt = 'profilePic';
+  card.appendChild(imageEl);
+
+  // create name for employee
+  let name = document.createElement('h5');
+  // name.classList = ('card-title');
+  name.textContent = this.fullName;
+  card.appendChild(name);
+
+  // create Department, Level, salary, Id for employee
+  let paragraph = document.createElement('p');
+  // paragraph.classList = ('card-text');
+  paragraph.textContent = `ID: ${this.employeeId}  \n - Department: ${this.department} \n - Level: ${this.level} \n - salary: ${this.salary} JD`;
+  card.appendChild(paragraph);
+
  
 }
-
-function netSalary(lev){
-
-    if(lev=="Senior"){
-            salary= (1500+2000)/2;
-        }else if(lev == "Mid-Senior"){
-           salary= (1000+1500)/2;
-        }else if(lev == "Junior"){
-            salary= (500+1000)/2;
-        }
-        else {salary= 0}
+function gitAllData(){
+  var x = document.getElementById("depart");
+  
 }
+let emp1=new Employee("1001","Ghazi Samer","Administration","Senior","./assest/Ghazi.jpg")
+let emp2=new Employee("1002","Lana Ali","Finance","Senior","./assest/Lana.jpg")
+let emp3=new Employee("1003","Tamara Ayoub","Marketing","Senior","./assest/Tamara.jpg")
+let emp4=new Employee("1004","Safi Walid","Administration","Mid-Senior","./assest/Safi.jpg")
+let emp5=new Employee("1005","Omar Zaid","Development","Senior","./assest/Omar.jpg")
+let emp6=new Employee("1006","Rana Saleh","Development","Jonior","./assest/Rana.jpg")
+let emp7=new Employee("1007","Hadi Ahmad","Finance","Mid-Senior","./assest/Hadi.jpg")
+
+
+let btn=document.getElementById("submit")
+btn.addEventListener("click",alt)
+let id=1007;
 
 function alt(){
-    
-    i =x.selectedIndex;
-    dept=x.options[i].text
-    level=x1.options[i].text
-    img= inputText.value
-    nameCard=inputName.value
-    netSalary(level)
-    id++;
-    arr.push({id:id,Name:nameCard,department:dept,Level:level,image:img,Salary:salary})
-    console.log(arr);
-    showEmp(arr)
+  
+  let i =document.getElementById("depart").selectedIndex;
+  let i2 =document.getElementById("level").selectedIndex;
+   let   dept=document.getElementById("depart").options[i].value
+   let   level=document.getElementById("level").options[i2].value
+   let   img= imgph.text
+   let   nameCard=fullName.text
+  //  let salary = Employee.prototype.salary();
+    //  allEmp.NewSalary11()
+    let salary = Employee.prototype.NewSalary11();
+      id++;
+      let newEmployee = new Employee(id, nameCard, dept, level, img, salary);
+      allEmp.push(newEmployee)
+      // allEmp.push("Employee",{id:id,Name:nameCard,department:dept,Level:level,image:img,Salary:salary})
+        //  newEmployee.render()
+        // allEmp.push({newEmployee})
+        for(let i=0 ; i<allEmp.length ; i++){
+          allEmp[i].NewSalary11();
+          allEmp[i].render();
+      }
 }
 
+// function showEmp(arr){
 
-
-    function dropList1() {
-         x = document.createElement("SELECT");
-        x.setAttribute("id", "mySelect");
-        sectionEl2.appendChild(x)
-
-        var ze = document.createElement("option");
-        ze.setAttribute("id", "Administrationopt");
-        var t = document.createTextNode("Select Department");
-        ze.appendChild(t);
-        document.getElementById("mySelect").appendChild(ze);
-      
-        var z = document.createElement("option");
-        z.setAttribute("id", "Administrationopt");
-        var t = document.createTextNode("Administration");
-        z.appendChild(t);
-        document.getElementById("mySelect").appendChild(z);
-
-        var z2 = document.createElement("option");
-        z2.setAttribute("id", "Marketingopt");
-        var t2 = document.createTextNode("Marketing");
-        z2.appendChild(t2);
-        document.getElementById("mySelect").appendChild(z2);
-
-        var z3 = document.createElement("option");
-        z3.setAttribute("id", "develop");
-        var t2 = document.createTextNode("Development");
-        z3.appendChild(t2);
-        document.getElementById("mySelect").appendChild(z3);
-
-        var z4 = document.createElement("option");
-        z4.setAttribute("id", "Finance");
-        var t2 = document.createTextNode("Finance");
-
-        z4.appendChild(t2);
-        document.getElementById("mySelect").appendChild(z4); 
-      }
-
-      function dropList2() {
-        x1 = document.createElement("SELECT");
-        x1.setAttribute("id", "mySelect2");
-        
-        sectionEl3.appendChild(x1)
-
-        var zw = document.createElement("option");
-        zw.setAttribute("admin", "Administrationopt");
-        var t = document.createTextNode("Select Level");
-        zw.appendChild(t);
-        document.getElementById("mySelect2").appendChild(zw);
-      
-        var z = document.createElement("option");
-        z.setAttribute("admin", "Administrationopt");
-        var t = document.createTextNode("Junior");
-        z.appendChild(t);
-        document.getElementById("mySelect2").appendChild(z);
-
-        var z2 = document.createElement("option");
-        z2.setAttribute("marketing", "Marketingopt");
-        var t = document.createTextNode("Mid-Senior");
-        z2.appendChild(t);
-        document.getElementById("mySelect2").appendChild(z2);
-
-        var z3 = document.createElement("option");
-        z2.setAttribute("dev", "develop");
-        var t = document.createTextNode("Senior");
-        z3.appendChild(t);
-        document.getElementById("mySelect2").appendChild(z3);
-        
-      }
-
-
-
-function showEmp(arr){
-
-    let sectionEl4=document.createElement("section")
-    main.appendChild(sectionEl4)
-    sectionEl4.style.backgroundColor="#0000"
-    sectionEl4.style.textAlign="center"
-    sectionEl4.style.width="300px"
-    sectionEl4.style.marginLeft="40%"
-    sectionEl4.style.backgroundColor="#860000"
+//     let sectionEl4=document.createElement("section")
+//     main.appendChild(sectionEl4)
+//     sectionEl4.style.backgroundColor="#0000"
+//     sectionEl4.style.textAlign="center"
+//     sectionEl4.style.width="300px"
+//     sectionEl4.style.marginLeft="40%"
+//     sectionEl4.style.backgroundColor="#860000"
        
         
-        var name=document.createElement("p")
-        var id=document.createElement("p")
-        var dept=document.createElement("p")
-        var level=document.createElement("p")
-        var salary=document.createElement("p")   
-        let img=document.createElement("img")
-        img.style.padding="20px"
-         img.style.width="150px"
-            for(var i=0 ; i<arr.length ; i++){
-               img.src=arr[i].image
-               sectionEl4.appendChild(img)
-               id.textContent="Id: "+arr[i].id
-               sectionEl4.appendChild(id)          
-               name.textContent="Name :"+arr[i].Name
-               sectionEl4.appendChild(name)
-               dept.textContent="Department :"+arr[i].department
-               sectionEl4.appendChild(dept)
-               level.textContent="Level :"+arr[i].Level
-               sectionEl4.appendChild(level)
-               salary.textContent="Salary :"+arr[i].Salary
-               sectionEl4.appendChild(salary)
+//         var name=document.createElement("p")
+//         var id=document.createElement("p")
+//         var dept=document.createElement("p")
+//         var level=document.createElement("p")
+//         var salary=document.createElement("p")   
+//         let img=document.createElement("img")
+//         img.style.padding="20px"
+//          img.style.width="150px"
+//             for(var i=0 ; i<arr.length ; i++){
+//                img.src=arr[i].image
+//                sectionEl4.appendChild(img)
+//                id.textContent="Id: "+arr[i].id
+//                sectionEl4.appendChild(id)          
+//                name.textContent="Name :"+arr[i].Name
+//                sectionEl4.appendChild(name)
+//                dept.textContent="Department :"+arr[i].department
+//                sectionEl4.appendChild(dept)
+//                level.textContent="Level :"+arr[i].Level
+//                sectionEl4.appendChild(level)
+//                salary.textContent="Salary :"+arr[i].Salary
+//                sectionEl4.appendChild(salary)
                
 
-            }
+//             }
 
-      }
-dropList1()
-dropList2()
-creatInputText()      
-createButton()
+//       }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(allEmp);
